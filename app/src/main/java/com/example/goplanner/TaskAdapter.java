88943e2ca1f2d8679f201537b3a774c0dfddb705
Maesa Ken Neobi Arief <maesaken.neobi@gmail.com>
2024-11-22 10:32:49 +0700

    task adapter methods

--- app/src/main/java/com/example/goplanner/TaskAdapter.java
+++ app/src/main/java/com/example/goplanner/TaskAdapter.java
@@ -1,4 +1,32 @@
 package com.example.goplanner;
 
-public class TaskAdapter {
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class TaskAdapter extends RecyclerView.Adapter<TaskAdapter.TaskHolder> {
+    @NonNull
+    @Override
+    public TaskAdapter.TaskHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull TaskAdapter.TaskHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    public class TaskHolder extends RecyclerView.ViewHolder{
+
+        public TaskHolder(@NonNull View itemView) {
+            super(itemView);
+        }
+    }
 }
