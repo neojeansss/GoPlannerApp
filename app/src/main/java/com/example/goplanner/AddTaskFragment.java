b020e39744e616d3af942e95583ac5993f83e8ef
HiChiron <haivalent@gmail.com>
2024-12-12 13:51:55 +0700

    Tambahin Fragment Baru

---
+++ app/src/main/java/com/example/goplanner/AddTaskFragment.java
@@ -0,0 +1,26 @@
+package com.example.goplanner;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class AddTaskFragment extends Fragment {
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_add_task, container, false);
+    }
+
+}
