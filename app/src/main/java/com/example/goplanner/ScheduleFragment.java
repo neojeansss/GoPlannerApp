990c92501439cf2b4213a17c3eb6da1248754315
HiChiron <haivalent@gmail.com>
2024-12-14 09:04:44 +0700

    Major Update Add Task

--- app/src/main/java/com/example/goplanner/ScheduleFragment.java
+++ app/src/main/java/com/example/goplanner/ScheduleFragment.java
@@ -2,6 +2,7 @@ package com.example.goplanner;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
@@ -9,6 +10,9 @@ import androidx.fragment.app.FragmentTransaction;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CalendarView;
+
+import com.google.android.material.button.MaterialButton;
 
 public class ScheduleFragment extends Fragment {
 
@@ -24,6 +28,34 @@ public class ScheduleFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_schedule, container, false);
+        View view = inflater.inflate(R.layout.fragment_schedule, container, false);
+
+        MaterialButton mainAddBtn = view.findViewById(R.id.mainAddBtn);
+
+        mainAddBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Fragment addTaskFragment = new AddTaskFragment();
+                FragmentManager fragmentManager = requireActivity().getSupportFragmentManager();
+                FragmentTransaction transaction = fragmentManager.beginTransaction();
+
+                transaction.replace(R.id.fragmentCV, addTaskFragment);
+                transaction.addToBackStack(null); // Tambahkan ke back stack untuk navigasi kembali
+                transaction.commit();
+            }
+        });
+
+
+
+        CalendarView calendarView = view.findViewById(R.id.calendarView2);
+        calendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+            @Override
+            public void onSelectedDayChange(@NonNull CalendarView view, int year, int month, int dayOfMonth) {
+                //tanggal berubah, set di variable
+            }
+        });
+
+        return view;
+
     }
 }
